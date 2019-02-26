938435ac44855a1f859a39a969fdb6ddb5c34e45
muammerhallac <muammerhallac@gmail.com>
2019-02-26 16:00:49 +0300

    New project added - refactoring sample

---
+++ FunctionTests/GeneratePrimesTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using GeneratePrimes;
+
+namespace FunctionTests
+{
+    [TestFixture]
+    public class GeneratePrimesTest
+    {
+        [Test]
+        public void TestPrimes()
+        {
+            int[] nullArray = PrimeGenerator.GeneratePrimeNumbers(0);
+            Assert.AreEqual(nullArray.Length, 0);
+            int[] minArray = PrimeGenerator.GeneratePrimeNumbers(2);
+            Assert.AreEqual(minArray.Length, 1);
+            Assert.AreEqual(minArray[0], 2);
+            int[] threeArray = PrimeGenerator.GeneratePrimeNumbers(3);
+            Assert.AreEqual(threeArray.Length, 2);
+            Assert.AreEqual(threeArray[0], 2);
+            Assert.AreEqual(threeArray[1], 3);
+            int[] centArray = PrimeGenerator.GeneratePrimeNumbers(100);
+            Assert.AreEqual(centArray.Length, 25);
+            Assert.AreEqual(centArray[24], 97);
+        }
+    }
+}
