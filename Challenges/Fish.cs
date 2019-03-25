fdf396d982f5bce000c9d5bff8ce3be93941c294
muammerhallac <muammerhallac@gmail.com>
2019-03-25 14:40:53 +0300

    Fish challenge added

---
+++ Challenges/Fish.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Challenges
+{
+    public class Fish
+    {
+        public static int FishLunch(int[] A, int[] B)
+        {
+            if (!A.Any() || !B.Any())
+                return 0;
+
+            var upStreamFishes = new Stack<int>();
+            var downStreamFishes = new Stack<int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (B[i] == 0) //upstream fish
+                {
+                    if (downStreamFishes.Any())
+                    {
+                        while (downStreamFishes.Any() && downStreamFishes.Peek() < A[i])
+                        {
+                            downStreamFishes.Pop();
+                        }
+
+                        if (!downStreamFishes.Any())
+                        {
+                            upStreamFishes.Push(A[i]);
+                        }
+                    }
+                    else
+                        upStreamFishes.Push(A[i]);
+                }
+                else
+                    downStreamFishes.Push(A[i]);
+            }
+
+            return upStreamFishes.Count + downStreamFishes.Count;
+        }
+    }
+}
---
+++ FunctionTests/FishTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTests
+{
+    [TestFixture]
+    public class FishTests
+    {
+        [Test]
+        [TestCase(new int[] { 5, 1 }, new int[] { 0, 0 }, 2)]
+        [TestCase(new int[] { 4, 3, 2, 1, 5 }, new int[] { 0, 1, 0, 0, 0, }, 2)]
+        public void FishLunch_WhenCalled_ShouldReturnDesiredResult(int[] A, int[] B, int desiredResult)
+        {
+            var result = Challenges.Fish.FishLunch(A, B);
+            Assert.That(result, Is.EqualTo(desiredResult));
+        }
+    }
+}
