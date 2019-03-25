145f748c2ba84dceab5622d6aebf6bbba0a65f23
muammerhallac <muammerhallac@gmail.com>
2019-03-25 15:39:28 +0300

    Brackets challenge added

---
+++ Challenges/Brackets.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class Brackets
+    {
+        public static int IsNested(string S)
+        {
+            if (string.IsNullOrEmpty(S) || S.Length % 2 != 0)
+                return 0;
+
+            var firstPartOfString = S.Substring(0, S.Length / 2);
+            var secondPartOfString = S.Substring(S.Length / 2, S.Length / 2);
+            var mismatchParts = new Stack<int>();
+            var reversedSecondPart = secondPartOfString.ToCharArray();
+            Array.Reverse(reversedSecondPart);
+
+            for (int i = 0; i < firstPartOfString.Length; i++)
+            {
+                var currentCharacter = firstPartOfString[i];
+                switch (currentCharacter)
+                {
+                    case '(':
+                        if (reversedSecondPart[i] != ')')
+                            mismatchParts.Push(1);
+                        break;
+                    case '[':
+                        if (reversedSecondPart[i] != ']')
+                            mismatchParts.Push(1);
+                        break;
+                    case '{':
+                        if (reversedSecondPart[i] != '}')
+                            mismatchParts.Push(1);
+                        break;
+                    case ')':
+                        if (reversedSecondPart[i] != '(')
+                            mismatchParts.Push(1);
+                        break;
+                    case ']':
+                        if (reversedSecondPart[i] != '[')
+                            mismatchParts.Push(1);
+                        break;
+                    case '}':
+                        if (reversedSecondPart[i] != '{')
+                            mismatchParts.Push(1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return mismatchParts.Count > 0 ? 0 : 1;
+        }
+    }
+}
---
+++ FunctionTests/BracketsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTests
+{
+    [TestFixture]
+    public class BracketsTests
+    {
+        [Test]
+        [TestCase("", 0)]
+        [TestCase("()[", 0)]
+        [TestCase("()", 1)]
+        [TestCase("[]", 1)]
+        [TestCase("{}", 1)]
+        [TestCase("([])", 1)]
+        [TestCase("{[()()]}", 1)]
+        [TestCase("([)()]", 0)]
+        public void IsNested_WhenCalled_ShouldReturnDesiredResult(string S, int desiredResult)
+        {
+            var result = Challenges.Brackets.IsNested(S);
+            Assert.That(result, Is.EqualTo(desiredResult));
+        }
+    }
+}
