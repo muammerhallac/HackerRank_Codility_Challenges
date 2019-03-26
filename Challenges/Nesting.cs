db3495e730c4534e18e805d2a2c77430bc1a3364
muammerhallac <muammerhallac@gmail.com>
2019-03-26 09:22:41 +0300

    Nesting challenge added

---
+++ Challenges/Nesting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class Nesting
+    {
+        public static int IsNestingString(string S)
+        {
+            if (string.IsNullOrEmpty(S))
+                return 1;
+
+            while (S.IndexOf("()") >= 0)
+            {
+                S = S.Replace("()", string.Empty);
+            }
+
+            return S.Length > 0 ? 0 : 1;
+        }
+    }
+}
---
+++ FunctionTests/NestingTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTests
+{
+    [TestFixture]
+    public class NestingTests
+    {
+        [Test]
+        [TestCase("", 1)]
+        [TestCase("())", 0)]
+        [TestCase("()", 1)]
+        [TestCase("(())", 1)]
+        [TestCase("(()(())())",1)]
+        [TestCase("((())(())(()))", 1)]
+        public void IsNestingString_WhenCalled_ShouldReturnDesiredResult(string S, int desiredResult)
+        {
+            int result = Challenges.Nesting.IsNestingString(S);
+            Assert.That(result, Is.EqualTo(desiredResult));
+        }
+    }
+}
