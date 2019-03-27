174854e5d5cc34c767e7aff51b5ed025d9969b33
muammerhallac <muammerhallac@gmail.com>
2019-03-27 12:42:01 +0300

    Dominator Challenge added

---
+++ Challenges/Dominator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Challenges
+{
+    public class Dominator
+    {
+        public static object CalculateDominator(int[] A)
+        {
+            if (!A.Any())
+                return -1;
+
+            var indexNumber = -2;
+            var dominatorCount = A.Count() / 2;
+            var groupedItems = A.GroupBy(x => x);
+            var dominatorGroup = groupedItems.Where(x => x.Count() > dominatorCount);
+            if (dominatorGroup.Count() <= 0)
+                return -1;
+            indexNumber = Array.IndexOf(A, dominatorGroup.FirstOrDefault().Key);
+
+            return indexNumber;
+        }
+    }
+}
---
+++ FunctionTests/DominatorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTests
+{
+    [TestFixture]
+    public class DominatorTests
+    {
+        [Test]
+        [TestCase(new int[] { }, -1)]
+        [TestCase(new int[] { 1, 2 }, -1)]
+        [TestCase(new int[] { 1, 1 }, 0)]
+        public void CalculateDominator_WhenCalled_ShouldReturnDesiredResult(int[] arr, int desiredResult)
+        {
+            var result = Challenges.Dominator.CalculateDominator(arr);
+            Assert.That(result, Is.EqualTo(desiredResult));
+        }
+    }
+}
