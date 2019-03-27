c8450ea0d7d44efde6811445ed68db1159cd2ffc
muammerhallac <muammerhallac@gmail.com>
2019-03-27 08:25:51 +0300

    StringReversing

---
+++ Challenges/StringReversing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class StringReversing
+    {
+        public static string ReverseString(string input)
+        {
+            if (input == "")
+                return input;
+
+            //var charArray = input.ToCharArray();
+            //Array.Reverse(charArray);
+
+            //return new string(charArray);
+
+            var reversedString = string.Empty;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedString = reversedString + input[i];
+            }
+
+            return reversedString;
+        }
+    }
+}
---
+++ FunctionTests/StringReversingTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTests
+{
+    [TestFixture]
+    public class StringReversingTests
+    {
+        [Test]
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCase("aa", "aa")]
+        [TestCase("ab", "ba")]
+        [TestCase("ab1", "1ba")]
+        public void ReverseString_WhenCalled_ShouldReturnDesiredResult(string input, string desiredResult)
+        {
+            string result = Challenges.StringReversing.ReverseString(input);
+            Assert.That(result, Is.EqualTo(desiredResult));
+        }
+    }
+}
